264acff12852cac14599bd253905f7a9372f1cff
sebastnchan <vauntex18@gmail.com>
2021-01-20 17:18:57 -0300

    FileManager + resources.

--- bungee/src/main/java/club/rigox/bungee/PixelMOTD.java
+++ bungee/src/main/java/club/rigox/bungee/PixelMOTD.java
@@ -1,14 +1,32 @@
 package club.rigox.bungee;
 
+import club.rigox.bungee.utils.FileManager;
 import net.md_5.bungee.api.plugin.Plugin;
 
 public final class PixelMOTD extends Plugin {
+    public static PixelMOTD instance;
 
     @Override
     public void onEnable() {
+        instance = this;
+
+        loadConfigs();
     }
 
     @Override
     public void onDisable() {
+
+    }
+
+    public void loadConfigs() {
+        FileManager manager = new FileManager(this);
+
+        manager.loadConfig("command");
+        manager.loadConfig("edit");
+        manager.loadConfig("modules");
+        manager.loadConfig("normal-motd");
+        manager.loadConfig("settings");
+        manager.loadConfig("timer-motd");
+        manager.loadConfig("whitelist-motd");
     }
 }
---
+++ bungee/src/main/java/club/rigox/bungee/utils/FileManager.java
@@ -0,0 +1,64 @@
+package club.rigox.bungee.utils;
+
+import club.rigox.bungee.PixelMOTD;
+import net.md_5.bungee.config.Configuration;
+import net.md_5.bungee.config.ConfigurationProvider;
+import net.md_5.bungee.config.YamlConfiguration;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+
+import static club.rigox.bungee.utils.Logger.warn;
+
+public class FileManager {
+    private final PixelMOTD plugin;
+
+    public FileManager (PixelMOTD plugin) {
+        this.plugin = plugin;
+    }
+    /**
+     * Creates a config File if it doesn't exists,
+     * reloads if specified file exists.
+     *
+     * @param configName config to create/reload.
+     * @return created/reloaded config if exists.
+     */
+    public Configuration loadConfig(String configName) {
+        File configFile = new File(plugin.getDataFolder().getPath(), configName + ".yml");
+
+        if (!configFile.exists()) {
+            plugin.getDataFolder().mkdirs();
+            saveConfig(configName);
+        }
+
+        Configuration cnf = null;
+        try {
+            cnf = ConfigurationProvider.getProvider(YamlConfiguration.class).load(configFile);
+        } catch (Exception e) {
+            warn(String.format("A error occurred while loading the settings file. Error: %s", e));
+            e.printStackTrace();
+        }
+        return cnf;
+    }
+
+
+    public void saveConfig(String configName) {
+        File folderDir = plugin.getDataFolder();
+        File file      = new File(plugin.getDataFolder(), configName + ".yml");
+
+        if (!folderDir.exists()) {
+            folderDir.mkdir();
+        }
+
+        if (!file.exists()) {
+            try (InputStream in = plugin.getResourceAsStream(configName + ".yml")) {
+                Files.copy(in, file.toPath());
+            } catch (IOException e) {
+                warn(String.format("A error occurred while copying the config %s to the plugin data folder. Error: %s", configName, e));
+                e.printStackTrace();
+            }
+        }
+    }
+}
