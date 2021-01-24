54861cc7885f9cc4394ddc3059636eb062490de6
sebastnchan <vauntex18@gmail.com>
2021-01-24 13:37:43 -0300

    Whitelist & Blacklist event done

--- bungee/src/main/java/club/rigox/bungee/listeners/WhitelistEvent.java
+++ bungee/src/main/java/club/rigox/bungee/listeners/WhitelistEvent.java
@@ -1,11 +1,15 @@
 package club.rigox.bungee.listeners;
 
 import club.rigox.bungee.PixelMOTD;
+import net.md_5.bungee.api.ChatColor;
+import net.md_5.bungee.api.chat.TextComponent;
 import net.md_5.bungee.api.event.LoginEvent;
 import net.md_5.bungee.api.plugin.Listener;
 import net.md_5.bungee.event.EventHandler;
 
 import java.util.List;
+
+import static club.rigox.bungee.utils.Logger.color;
 
 public class WhitelistEvent implements Listener {
     private final PixelMOTD plugin;
@@ -24,19 +28,38 @@ public class WhitelistEvent implements Listener {
 
         List<String> whitelistPlayer = plugin.getEditableFile().getStringList("whitelist.players-name");
         List<String> whitelistUuid   = plugin.getEditableFile().getStringList("whitelist.players-uuid");
-        List<String> kickMessage     = plugin.getEditableFile().getStringList("whitelist.kick-message");
+        List<String> whitelistMsg    = plugin.getEditableFile().getStringList("whitelist.kick-message");
 
         boolean whitelistCheck       = plugin.getEditableFile().getString("whitelist.check-mode").equalsIgnoreCase("LoginEvent");
-        boolean whitelistToggle      = plugin.getEditableFile()     .getBoolean("whitelist.toggle");
+        boolean whitelistToggle      = plugin.getEditableFile().getBoolean("whitelist.toggle");
 
         if (whitelistCheck && whitelistToggle) {
-            if (!whitelistPlayer.contains(connectionName) || !whitelistUuid.contains(connectionUuid)) {
-                String kickReason = plugin.getConverter().fromListToString(kickMessage);
-//                e.setCancelReason(new TextComponent(ChatColor.translateAlternateColorCodes('&', kickReason.replace("%whitelist_author%", getWhitelistAuthor()).replace("%type%", "Server"))));
-//                e.setCancelReason(new TextComponent(color(kickReason.replace("%whitelist_author%", get))));
-            }
+            if (!whitelistPlayer.contains(connectionName) && !whitelistUuid.contains(connectionUuid)) {
+                String kickReason = plugin.getConverter().fromListToString(whitelistMsg);
 
+                e.setCancelReason(new TextComponent(color(kickReason
+                        .replace("%whitelist_author%", plugin.getPlaceholders().getWhitelistAuthor())
+                        .replace("%type%", "Server"))));
+            }
+            return;
         }
 
+        List<String> blacklistPlayer = plugin.getEditableFile().getStringList("blacklist.players-name");
+        List<String> blacklistUuid   = plugin.getEditableFile().getStringList("blacklist.players-uuid");
+        List<String> blacklistMsg    = plugin.getEditableFile().getStringList("blacklist.kick-message");
+
+        boolean blacklistToggle      = plugin.getEditableFile().getBoolean("blacklist.toggle");
+
+        if (blacklistToggle) {
+            if (blacklistPlayer.contains(connectionName) || blacklistUuid.contains(connectionUuid)) {
+                String kickReason = plugin.getConverter().fromListToString(blacklistMsg);
+
+                e.setCancelReason(new TextComponent(color(kickReason
+                        .replace("%nick%", connectionName))
+                        .replace("%type%", "Server")));
+            }
+        }
+
+        // TODO Block words in name
     }
 }
