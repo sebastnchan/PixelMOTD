9297abec1cecf8663f7dc4d88a709f14f4f1742d
sebastnchan <vauntex18@gmail.com>
2021-02-04 12:24:30 -0300

    New hex color working fine :ok_hand:

--- bungee/src/main/java/club/rigox/bungee/utils/NewHexColor.java
+++ bungee/src/main/java/club/rigox/bungee/utils/NewHexColor.java
@@ -5,11 +5,10 @@ import net.md_5.bungee.api.ChatColor;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import static club.rigox.bungee.utils.Logger.color;
 import static club.rigox.bungee.utils.Logger.debug;
 
 public class NewHexColor {
-    private static final Pattern HEX = Pattern.compile("#[a-fA-F0-9]{6}");
+    private static final Pattern HEX = Pattern.compile("<#[a-fA-F0-9]{6}>");
 
     public String formatToHex(String message) {
         Matcher match = HEX.matcher(message);
@@ -21,6 +20,6 @@ public class NewHexColor {
         }
 
         debug("formatToHex method was called.");
-        return color(message);
+        return ChatColor.translateAlternateColorCodes('&', message);
     }
 }
